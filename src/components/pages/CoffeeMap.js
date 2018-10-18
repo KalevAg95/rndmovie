9ef5bda0f9fc144479f4d68497d8ae3826ac2ba3
Kalev Aguirre <kalevag95@gmail.com>
2018-10-18 16:04:50 -0500

    nearby search working

--- src/components/pages/CoffeeMap.js
+++ src/components/pages/CoffeeMap.js
@@ -1,27 +1,61 @@
 import React from 'react';
 import GoogleMapReact from 'google-maps-react';
-import  { Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import  { Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 
 class CoffeeMap extends React.Component{	
  	
+ 	
+	state = {
+		userLocation: { lat: 50, lng: 500 }, 
+		loading: true
+	};
 
 	componentDidMount(props) {
     	navigator.geolocation.getCurrentPosition(
-      	position => {
-        	const { latitude, longitude } = position.coords;
-        	this.setState({
-          	userLocation: { lat: latitude, lng: longitude },
-          	loading: false
-        	});
-      	},
-      	() => {
+      		position => {
+        		const { latitude, longitude } = position.coords;
+
+        			this.setState({
+          			userLocation: { lat: latitude, lng: longitude },
+          			loading: false,
+          			
+        		});
+      		}, () => {
         		this.setState({ loading: false });
       		}
     	);
   	}
 
+  	located = (userLocation) => {
+  		console.log(userLocation);
+  	}
+
+  	
+
+  	fetchPlaces = (mapProps, map) =>{
+  		const {google} = mapProps;
+  		const service = new window.google.maps.places.PlacesService(map);
+
+  		const request = {
+                    location: this.state.userLocation,
+                    radius: 5000,
+                    type: ['cafe'],
+                };
+
+  		service.nearbySearch(request, (results, status) => {
+  				console.log(results);
+
+  			}
+  		);
+  	}	
+
+  	
+  			
 	render(){
+
+		const {google} =this.props;
+		const {loading, userLocation} = this.state;
 		
 		let mapStyle ={
 			height:"70%",
@@ -29,18 +63,21 @@ class CoffeeMap extends React.Component{
 			marginTop: "4rem",
 		}
 	
+		if(loading){
+			return null;
+		}
+
 		return(
 			<div>
 				<Map 
-					google={this.props.google} 
-					zoom={15} 
+					className='map'
+					google={google} 
+					zoom={15}
 					style={mapStyle}
-					initialCenter={
-						userLocation	
-					}
-					onReady={this.fetchPlaces}
-					visible={false}>
-					<Marker/>
+					initialCenter={userLocation}
+					onReady={this.located, this.fetchPlaces}
+					>
+
 				</Map>
 			</div>
 		);
